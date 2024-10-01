c72e867882632fa3642a3c75a164238f4c5cf3c8
Song JiWoong <016vhsdkdl@gmail.com>
2024-10-01 16:17:24 +0900

    IoC practice

---
+++ Ex02_IoC-1/src/main/java/com/sample/spring/Ex02IoC1Application.java
@@ -0,0 +1,13 @@
+package com.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex02IoC1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex02IoC1Application.class, args);
+	}
+
+}
